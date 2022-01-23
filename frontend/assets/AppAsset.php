3ffe46f3a5bf1cec07dc712d6aa79a1f110e09c4
SanakulovDev <emailkerak@gmail.com>
2022-01-23 21:44:12 +0500

    edit template

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,11 +13,23 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        "css/bootstrap.min.css",
+        "css/style.css",
+        "css/colors.css",
+        "css/versions.css",
+        "css/responsive.css",
+        "css/custom.css"
     ];
     public $js = [
+        "js/modernizer.js",
+        "js/all.js",
+        "js/custom.js",
+        "https://maps.googleapis.com/maps/api/js?key=AIzaSyCNUPWkb4Cjd7Wxo-T4uoUldFjoiUA1fJc&callback=myMap",
+
+        
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap4\BootstrapAsset',
+        // 'yii\bootstrap4\BootstrapAsset',
     ];
 }
